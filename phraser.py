2153c89ccdf5d101a9626984236dee2c12934351
Rory Molyneux <rory@pitchdark.co.za>
2019-05-22 19:28:30 +0200

    Phraser now creates a list of possible options based on the input and "self.links"

--- phraser.py
+++ phraser.py
@@ -5,57 +5,69 @@ class Phraser():
 
     def __init__ (self):
         # Going to keep things simple with these three word types
-        self.allwords = []
+        self.alltitles = []
         self.adjectives = []
         self.verbs = []
         self.nouns = []
-        self.other = [] # just in case
+        self.links = ["the", "in", "a", "has", "by", "are", "my", "and", "on", "an", "of", "to", "as", "with", "i", "have"] # linking phrase words, conjunctions, prepositions etc.
+        self.phrases = []
 
     def read(self, filename):
 
         f = open(filename, 'r')
 
-        self.allwords = re.sub("\n", " \n", f.read()).lower().replace("\n", "").split(' ')
-        # no idea why i suddenly need to replace \n
+        # split out each individual song title from the text file
+        self.alltitles = f.read().lower().split("\n")
 
-        return self.allwords
+        return self.alltitles
 
     def sort(self):
 
-        for word in self.allwords:
-            word = word.replace('.', '')
+        for title in self.alltitles:
 
-            # this is a hyper simple approach, need more logic here
-            if word.endswith('ing'):
-                self.verbs.append(word)
-            elif word.endswith('ed'):
-                self.verbs.append(word)
-            elif word.endswith('ly'):
-                self.adjectives.append(word)
-            elif word in ["the", "in", "a", "by", "my", "and", "on", "an", "of", "to", "as", "with", "i"]:
-                word
-            else:
-                self.nouns.append(word)
+            explodedtitle = title.split(" ")
+            titlephrase = []
+
+            for word in explodedtitle:
+
+                word = word.replace('.', '')
+
+                if word in self.links:
+                    titlephrase.append(word)
+                elif word.endswith('ing'):          # put doing words in verbs
+                    self.verbs.append(word)
+                    titlephrase.append('%v')
+                elif word.endswith('ed'):           # put 'ed words in verbs
+                    self.verbs.append(word)
+                    titlephrase.append('%v')
+                elif word.endswith('ly'):           # put 'ly words in adjectives (fuzzy)
+                    self.adjectives.append(word)
+                    titlephrase.append('%a')
+                else:
+                    self.nouns.append(word)         # everything else is a noun 'cos that's how english works
+                    titlephrase.append('%n')
+
+            if titlephrase not in self.phrases:
+                self.phrases.append(' '.join(titlephrase))
 
     def generate(self):
 
-        phrases = [
-            "%v by %n",
-            "%v in the %n",
-            "%v %n",
-            "%v %a by %n",
-            "%v with %n",
-            "%n %v",
-            "the %a %n",
-            "a %n"
-        ]
-        phrase = random.choice(phrases)
+        phrase = random.choice(self.phrases)
 
-        phrase = phrase.replace("%v", random.choice(self.verbs))\
-                       .replace("%n", random.choice(self.nouns))\
-                       .replace("%a", random.choice(self.adjectives))
+        thephrase = phrase.split(' ')
 
-        return phrase.upper()
+        for key, word in enumerate(thephrase):
+
+            if word == '%n':
+                thephrase[key] = random.choice(self.nouns)
+            elif word == '%a':
+                thephrase[key] = random.choice(self.adjectives)
+            elif word == '%v':
+                thephrase[key] = random.choice(self.verbs)
+            else:
+                thephrase[key] = word
+
+        return ' '.join(thephrase).upper()
 
 
 
